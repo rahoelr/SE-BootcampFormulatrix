4eced031d4cc82f40d9526462bbc8e3689b0c0f9
Rahul Rahmatullah <rahul.rahmatullah@mail.ugm.ac.id>
2026-01-13 10:35:25 +0700

    feat: add foobar function

---
+++ FooBar/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+FooBar(20);
+
+static void FooBar(int n)
+{
+    for (int i = 1; i < n+1; i++)
+    {
+        if (i%3 == 0 && i%5 == 0)
+        {
+            Console.WriteLine("FooBar");
+        } else if (i%3==0)
+        {
+            Console.WriteLine("Foo");
+        } else if (i % 5 == 0)
+        {
+            Console.WriteLine("Bar");
+        } else
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
