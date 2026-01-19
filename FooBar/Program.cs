7c0e9de8275a80738c48b5d45825bf2bbaef9530
Rahul Rahmatullah <rahul.rahmatullah@mail.ugm.ac.id>
2026-01-19 08:11:04 +0700

    fix: update foo bar jazz using hardcode method

--- FooBar/Program.cs
+++ FooBar/Program.cs
@@ -1,12 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-FooBar(15);
+FooBar(100);
 
 static void FooBar(int n)
 {
     for (int i = 1; i < n+1; i++)
     {
-        if (i%3 == 0 && i%5 == 0)
+        if (i%3 ==0 && i%5 ==0 && i%7==0)
+        {
+            Console.WriteLine("FooBarJazz");
+        } else if (i%3 ==0 && i%7==0)
+        {
+            Console.WriteLine("FooJazz");
+        } else if (i%5 ==0 && i%7==0)
+        {
+            Console.WriteLine("BarJazz");
+        }
+         else if (i%3 == 0 && i%5 == 0)
         {
             Console.WriteLine("FooBar");
         } else if (i%3==0)
